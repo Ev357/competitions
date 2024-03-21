8e7e6b3479a0846f2d48c85587fe6e67dd798b2c
Ev357 <evest.dev@gmail.com>
2024-03-21 11:02:21 +0100

    feat: generator

--- alcedo-39/scope/generator/src/index.ts
+++ alcedo-39/scope/generator/src/index.ts
@@ -1,53 +1,58 @@
-import z from "zod";
+// eslint-disable-next-line import/no-named-as-default
 import prompts from "prompts";
 
+const questions = [
+  {
+    type: "number",
+    name: "vzorkovani",
+    message: "Vzorkování: ",
+  },
+  {
+    type: "number",
+    name: "delkaZaznamu",
+    message: "Délka záznamu: ",
+  },
+  {
+    type: "number",
+    name: "rozsah",
+    message: "Rozsah: ",
+  },
+];
+
+const generateVoltageSamples = (
+  sampleCount: number,
+  minValue: number,
+  maxValue: number,
+) => {
+  const frequency = Math.random() * 10 + 1;
+  const phase = Math.random() * Math.PI * 2;
+  const amplitude = (maxValue - minValue) / 2;
+
+  const voltageSamples: number[] = [];
+  for (let i = 0; i < sampleCount; i++) {
+    const time = i / sampleCount;
+    let voltage = amplitude * Math.sin(2 * Math.PI * frequency * time + phase);
+
+    voltage += ((Math.random() - 0.5) * (maxValue - minValue)) / 10;
+
+    voltage = Math.min(Math.max(voltage, minValue), maxValue);
+
+    voltageSamples.push(voltage);
+  }
+  return voltageSamples;
+};
+
 (async () => {
-  let response = await prompts({
-    type: "text",
-    name: "text",
-    message: "Vyber soustavu: ",
-  });
-  const soustavaSchema = z.number().min(2).max(16);
-  const soustavaResult = soustavaSchema.safeParse(+response.text.trim());
-  let soustava: number;
-  if (!soustavaResult.success) {
-    throw "Špatný input";
-  }
-  soustava = soustavaResult.data;
+  const response = await prompts(questions as any);
+  const pocet = response.vzorkovani * response.delkaZaznamu;
+  const voltageSamples = generateVoltageSamples(
+    pocet,
+    -response.rozsah,
+    response.rozsah,
+  );
 
-  response = await prompts({
-    type: "text",
-    name: "text",
-    message: "A: ",
-  });
-  let a: number;
+  let outFileText = `${response.vzorkovani}\n${response.delkaZaznamu}\n${response.rozsah}\n`;
+  outFileText += voltageSamples.join("\n");
 
-  const aResult = parseInt(response.text.trim(), soustava);
-  if (Number.isNaN(aResult)) {
-    throw "Špatný input";
-  }
-  a = aResult;
-
-  console.log(a);
-
-  // -----
-
-  response = await prompts({
-    type: "text",
-    name: "text",
-    message: "B: ",
-  });
-  let b: number;
-
-  const bResult = parseInt(response.text.trim(), soustava);
-  if (Number.isNaN(aResult)) {
-    throw "Špatný input";
-  }
-  b = bResult;
-
-  const decimalResult = a + b;
-  const finalResult = decimalResult.toString(soustava);
-
-  console.log(`Desítková soustava: ${decimalResult}`);
-  console.log(`${soustava} soustava: ${finalResult.toUpperCase()}`);
+  await Bun.write("MERENI.txt", outFileText);
 })();
